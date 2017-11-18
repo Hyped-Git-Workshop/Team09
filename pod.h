d5d5ede766ebdf631246a367719f8b181443f989
Abhay <abhaydais@gmail.com>
2017-11-18 14:49:32 +0000

    Added pressure

--- pod.h
+++ pod.h
@@ -15,14 +15,15 @@ class Pod
 
     std::string get_name();
     double get_max_speed();
-    /// TASKS 3 (PERSON A) & 4 (PERSON B) 
+    /// TASKS 3 (PERSON A) & 4 (PERSON B)
 double get_temperature();
-    
+
+double get_pressure();
     /// END OF TASKS 3A & 4B ///////////////////////////////////////////////////
 
 
     /// TASK 10.2 (BOTH) ///////////////////////////////////////////////////////
-    
+
     /// END OF TASK 10.2 (BOTH) ////////////////////////////////////////////////
 };
 
